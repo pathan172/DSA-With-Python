2a935630c85c778541b270670540913d3f22ff03
Anzar Khan <anzark2914@gmai,com>
2026-07-31 03:11:16 +0530

    day1:python basics

---
+++ day1.py
@@ -0,0 +1,20 @@
+#we want to count of digits in a given string 
+# num = input("Enter a string: ")
+# n = int(num)
+# count = 0 
+# while n > 0:
+#     count += 1
+#     n = n // 10
+# print(count)
+
+
+
+#check if number is palindrome or not
+num = int(input("Enter a number: "))
+num = int(num)
+result = 0
+while num > 0:
+    ld = num %10
+    result = result *10 +ld
+    num = num //10
+print(num == result)
